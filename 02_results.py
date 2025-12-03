f7a62b0255b1388bb2189cbd76af73354f640d76
Jeremia Kimelman <jbkimelman@gmail.com>
2025-12-03 14:35:27 -0800

    Add Butte and Contra Costa

--- 02_results.py
+++ 02_results.py
@@ -1,6 +1,6 @@
 import marimo
 
-__generated_with = "0.18.0"
+__generated_with = "0.18.1"
 app = marimo.App(width="medium")
 
 
@@ -28,11 +28,136 @@ def _():
 
 
 @app.cell
-def _(imperial, pd):
-    combined = pd.concat([imperial])
+def _(butte, contra_costa, imperial, pd):
+    combined = pd.concat([butte, contra_costa, imperial]).reset_index(drop=True)
     combined.to_csv("outputs/results.csv", index=False)
     combined.head()
     return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## Butte
+    """)
+    return
+
+
+@app.cell
+def _(pd):
+    def butte_df():
+        csv = pd.read_excel(
+            "inputs/counties/butte/detail.xlsx", sheet_name="2", skiprows=2
+        )
+        csv.rename(
+            columns={
+                "Precinct": "precinct_id",
+                "Total Votes": "no_votes",
+                "Total Votes.1": "yes_votes",
+            },
+            inplace=True,
+        )
+        csv["county"] = "Butte"
+        csv["turnout"] = (csv["Total"] / csv["Registered Voters"]) * 100
+        csv.drop(
+            columns=[
+                "Live",
+                "Vote By Mail",
+                "Registered Voters",
+                "Provisional",
+                "Live.1",
+                "Vote By Mail.1",
+                "Provisional.1",
+                "Total",
+            ],
+            inplace=True,
+        )
+        return csv
+
+
+    butte = butte_df()
+    butte.head()
+    return (butte,)
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## Contra Costa
+    """)
+    return
+
+
+@app.cell
+def _(pd):
+    def contra_costa_df():
+        csv = pd.read_excel(
+            "inputs/counties/contra_costa/StatementOfVotesCastRPT_ByPrecinct.xlsx",
+            sheet_name="Sheet2",
+            skiprows=3,
+        )
+
+        # add the county
+        csv["county"] = "Contra Costa"
+
+        # drop some columns we don't need
+        csv.drop(
+            columns=[
+                "Times Cast",
+                "Unnamed: 3",
+                "Precinct.1",
+                "Unnamed: 6",
+                "Unnamed: 8",
+                "Unnamed: 10",
+                "Unnamed: 11",
+                "Unnamed: 12",
+            ],
+            inplace=True,
+        )
+
+        # there are four rows per precinct so let's drop two of them
+        # "In-person" and "Vote By Mail"
+        csv.drop(csv[csv["Precinct"] == "In-Person"].index, inplace=True)
+        csv.drop(csv[csv["Precinct"] == "Vote By Mail"].index, inplace=True)
+
+        # get rid of the first two rows
+        csv.drop([0, 1], inplace=True)
+
+        # backfill the values from the rows where "Precinct" is "Total" to the rows
+        # that have proper precinct IDs
+        csv.bfill(limit=1, inplace=True)
+
+        # and now get rid of that extra row for Total
+        csv.drop(csv[csv["Precinct"] == "Total"].index, inplace=True)
+        csv.rename(
+            columns={
+                "Precinct": "precinct_id",
+                "Yes\n ": "yes_votes",
+                "No\n ": "no_votes",
+            },
+            inplace=True,
+        )
+
+        # replaced masked values with 0 for turnout calculations
+        csv.replace("****", 0, inplace=True)
+
+        # and then add in a calculated turnout column
+        csv["turnout"] = (csv["Total Votes"] / csv["Registered \nVoters"]) * 100
+
+        # make the index a column so we can drop it
+        csv.reset_index(inplace=True)
+
+        # get rid of remaining columns
+        csv.drop(
+            columns=["index", "Registered \nVoters", "Total Votes"], inplace=True
+        )
+
+        return csv
+
+
+    contra_costa = contra_costa_df()
+    contra_costa.head(20)
+    return (contra_costa,)
 
 
 @app.cell(hide_code=True)
@@ -74,6 +199,8 @@ def _(pd):
         prop_50_altered["turnout"] = prop_50_altered["turnout"].str.replace(
             "%", ""
         )
+
+        prop_50_altered["county"] = "Imperial"
 
         return prop_50_altered
 
