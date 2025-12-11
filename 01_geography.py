5e949924df77c9db86cb1d06f5585478190dacac
Mohamed Al Elew <mohamed@themarkup.org>
2025-12-11 15:49:36 -0600

    47 remove zero voter precincts

--- 01_geography.py
+++ 01_geography.py
@@ -1,6 +1,6 @@
 import marimo
 
-__generated_with = "0.18.2"
+__generated_with = "0.18.4"
 app = marimo.App(width="medium")
 
 
@@ -360,6 +360,8 @@ def _(PROJECTED_CRS, gpd):
 def _(mo):
     mo.md(r"""
     ## Contra Costa
+
+    Precincts with zero registered voters are filtered out, because these precincts are not included in the official results data. [Read more issue #47](https://github.com/CalMatters/data-prop50-results/issues/47)
     """)
     return
 
@@ -369,6 +371,9 @@ def _(PROJECTED_CRS, gpd):
     contra_costa = gpd.read_file(
         "inputs/counties/contra_costa/precincts/PrecinctSet_PDMJ017.json"
     ).to_crs(PROJECTED_CRS)
+
+    has_voters = contra_costa["iZeroRegistrationPct"] != 1
+    contra_costa = contra_costa[has_voters].copy()
 
     contra_costa = alter_df(
         contra_costa,
