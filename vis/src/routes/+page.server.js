0dcc2fcf97b1a812b56dad48463d946c38211e8a
Jeremia Kimelman <jbkimelman@gmail.com>
2026-02-11 09:51:07 -0800

    searchParams.get() takes 1 argument in +page.server.js

--- vis/src/routes/+page.server.js
+++ vis/src/routes/+page.server.js
@@ -1,6 +1,6 @@
 /** @type {import('./$types').PageLoad} */
 export function load({ url }) {
-  const county = url.searchParams.get("county", undefined);
+  const county = url.searchParams.get("county"); // returns null if not found
 
   return {
     county,
