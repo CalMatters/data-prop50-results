f6bd514a572a7a732c6f7355c704d22807f0fe5d
Mohamed Al Elew <13629454+malelew@users.noreply.github.com>
2026-03-05 12:16:23 -0600

    Flipped precincts (#246)
    * Flipped precincts init
    
    * code and doc cleanup
    
    * edit md
    
    * Add partisan flipped field

--- 05_analyze_demographics.py
+++ 05_analyze_demographics.py
@@ -152,7 +152,7 @@ def _(display_options):
     return (config_data_options_multiselect,)
 
 
-@app.cell(hide_code=True)
+@app.cell
 def _():
     mo.md(r"""
     ## Filepaths
@@ -360,6 +360,26 @@ def _(
         precinct_2025_results["yes_pct"] - precinct_2025_results["dem_pct_2024"], 1
     )
 
+    # Identify partisan flip
+    is_trump_win = (
+        precinct_2025_results["rep_pct_2024"]
+        > precinct_2025_results["dem_pct_2024"]
+    )
+    is_harris_win = (
+        precinct_2025_results["rep_pct_2024"]
+        < precinct_2025_results["dem_pct_2024"]
+    )
+
+    is_prop50_win = (
+        precinct_2025_results["yes_pct"] > precinct_2025_results["no_pct"]
+    )
+    is_prop50_loss = (
+        precinct_2025_results["yes_pct"] < precinct_2025_results["no_pct"]
+    )
+
+    precinct_2025_results.loc[is_trump_win & is_prop50_win, "flipped"] = "D"
+    precinct_2025_results.loc[is_harris_win & is_prop50_loss, "flipped"] = "R"
+
     # Debug: county distribution of precincts with null yes_pct (validation opportunity)
     mo.vstack(
         [
@@ -369,7 +389,14 @@ def _(
             .value_counts(),
         ]
     )
-    return (precinct_results,)
+    return (
+        is_harris_win,
+        is_prop50_loss,
+        is_prop50_win,
+        is_trump_win,
+        precinct_2025_results,
+        precinct_results,
+    )
 
 
 @app.cell
@@ -843,6 +870,31 @@ def _(
 
 
 @app.cell(hide_code=True)
+def _(
+    is_harris_win,
+    is_prop50_loss,
+    is_prop50_win,
+    is_trump_win,
+    precinct_2025_results,
+):
+    mo.md(rf"""
+    ## Partisan flip
+
+    **There are {(is_trump_win & is_prop50_win).sum():,} precincts in the {precinct_2025_results["county"].nunique()} counties where Prop. 50 won and Trump won.** There were {(is_harris_win & is_prop50_loss).sum():,} precincts where Harris won but Prop. 50 lost.
+
+    These findings should be used only for exploratory purposes. The 2024 results are interpolated to 2025 precincts. This introduces issues such as the faulty assumption of uniform population distribution and the modifiable areal unit problem. These limitations require extra scrutiny of any findings we want to publish about partisan flips at the precinct-level.
+    """)
+    return
+
+
+@app.cell
+def _(precinct_2025_results):
+    flipped_precincts = precinct_2025_results[precinct_2025_results["flipped"].notnull()]
+    flipped_precincts
+    return
+
+
+@app.cell(hide_code=True)
 def _():
     mo.md(r"""
     ## Demographic scatterplot and linear regression
@@ -1223,11 +1275,6 @@ def _(MAP_EXPORT_CONFIG_KEY, precinct_results):
     _output_path = pathlib.Path("./outputs/county_precincts.json")
     with _output_path.open("w") as f:
         json.dump(county_precinct_dict, f)
-    return
-
-
-@app.cell
-def _():
     return
 
 
